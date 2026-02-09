308319b5ba5bcdf672100db996bd16b8594468a5
LAB24_USER ON WS14 <L24X07W14@LabHCI.net>
2026-02-09 10:12:07 +0800

    Initial commit: Spring Boot user authentication project

---
+++ userauth/src/main/java/com/abelgas/userauth/Controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.abelgas.userauth.controller;
+
+import com.abelgas.userauth.model.User;
+import com.abelgas.userauth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public String register(@RequestBody User user) {
+        userRepository.save(user);
+        return "User registered successfully";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        Optional<User> existingUser = userRepository.findByUsername(user.getUsername());
+        if (existingUser.isPresent() && existingUser.get().getPassword().equals(user.getPassword())) {
+            return "Login successful";
+        } else {
+            return "Invalid username or password";
+        }
+    }
+}
